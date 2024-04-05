1a2f3eea7c3eafd157d4c1abf753aa91db955b30
Jai Madera <onesthathumbled@gmail.com>
2024-04-05 21:14:31 +0800

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   get '/current_user', to: 'current_user#index'
   devise_for :traders
+
   devise_for :admins, path: '', path_names: {
     sign_in: 'admin/login',
     sign_out: 'admin/logout',
@@ -11,15 +12,20 @@ Rails.application.routes.draw do
     registrations: 'admins/registrations'
   }
   
-  # devise_for :users
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
+
+  # < -------------------------------------------------------------------------------------------- >
+  # For Testing Purposes
+  # devise_for :users
+
   devise_for :users, path: '', path_names: {
-    sign_in: 'login',
-    sign_out: 'logout',
-    registration: 'signup'
+    sign_in: 'user/login',
+    sign_out: 'user/logout',
+    registration: 'user/register'
   },
   controllers: {
     sessions: 'users/sessions',
